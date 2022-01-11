a83e283e7ccae82c2d755b00428ade79dafeec78
minhnqu97 <minhnq@monsterstudio.io>
2022-01-11 18:54:58 +0700

    Add Compute Shader

---
+++ Assets/Scripts/MultipleMask/AlphaConvert.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaConvert : MonoBehaviour
+{
+    public const string INPUT_TEXTURE = "InputTexture";
+    public const string OUTPUT_BUFFER = "OutputBuffer";
+    public ComputeShader m_computeShader;
+    int m_kernelIndex;
+    ComputeBuffer m_outputBuffer;
+
+    public float[] alpha;
+
+    public void Awake()
+    {
+
+        m_kernelIndex = m_computeShader.FindKernel("CSMain");
+    }
+
+    public float[] ConvertToFloatArray(Texture2D renderTexture)
+    {
+        m_computeShader.SetTexture(m_kernelIndex, INPUT_TEXTURE, renderTexture);
+        
+        float[] result = new float[renderTexture.width * renderTexture.height];
+        m_outputBuffer = new ComputeBuffer(result.Length, sizeof(float));
+        m_outputBuffer.SetData(result);
+        m_computeShader.SetBuffer(m_kernelIndex, OUTPUT_BUFFER, m_outputBuffer);
+
+        m_computeShader.Dispatch(m_kernelIndex, renderTexture.width / 30, renderTexture.height / 30, 1);
+
+        m_outputBuffer.GetData(result);
+
+        return result;
+    }
+    
+    
+    public void GetAplha(Texture2D renderTexture)
+    {
+        alpha = new float[renderTexture.width * renderTexture.height];
+        Color[] color = renderTexture.GetPixels();
+        for(int i = 0; i < renderTexture.width; ++i) 
+            for(int j = 0; j < renderTexture.height; ++j)
+            {
+                alpha[i + j * renderTexture.width] = color[i + j * renderTexture.width].a;
+            }
+    }
+}
---
+++ Assets/Scripts/MultipleMask/MultipleMask.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleMask : MonoBehaviour
+{
+    public Texture2D texture;
+    public ComputeShader combineMaskShader;
+    public AlphaConvert alphaConvert;
+    public MaskInfo mask;
+    public UITexture render;
+    public int boardSize = 10;
+    public int texResolution = 1080;
+    public string kernelName = "GetMask";
+
+    RenderTexture outputTexture;
+    private int kernelHandler;
+    JigsawMask[] jigsawMask;
+    ComputeBuffer buffer;
+    
+    public float[] alpha;
+
+    struct JigsawMask
+    {
+        public Color[] pixel;
+        public int id;
+    }
+
+    public void Start()
+    {
+        outputTexture = new RenderTexture(texResolution, texResolution, 0);
+        outputTexture.enableRandomWrite = true;
+        outputTexture.Create();
+        InitData();
+        InitShader();
+    }
+
+    private void InitData()
+    {
+        alpha = alphaConvert.ConvertToFloatArray(mask.texture);
+        
+    }
+
+    private void InitShader()
+    {
+        kernelHandler = combineMaskShader.FindKernel(kernelName);
+
+        combineMaskShader.SetInt("texResolution", texResolution);
+        combineMaskShader.SetInt("boardSize", boardSize);
+        buffer = new ComputeBuffer(alpha.Length, sizeof(float));
+        buffer.SetData(alpha);
+        combineMaskShader.SetBuffer(kernelHandler, "mask", buffer);
+        
+        combineMaskShader.SetTexture(kernelHandler, "Result", outputTexture);
+        DispatchKernels(1);
+        render.material.SetTexture("_Mask", outputTexture);
+    }
+
+
+
+    private void DispatchKernels(int count)
+    {
+        combineMaskShader.Dispatch(kernelHandler, count, 1, 1);
+    }
+
+
+    [MyBox.ButtonMethod]
+    public void SetAlpha()
+    {
+        alphaConvert.GetAplha(mask.texture);
+    }
+}
+
+[System.Serializable]
+public class MaskInfo
+{
+    public Texture2D texture;
+    public int id;
+}
